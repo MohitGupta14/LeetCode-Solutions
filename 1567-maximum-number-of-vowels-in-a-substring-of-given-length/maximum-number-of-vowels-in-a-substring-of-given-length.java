77d9f361fe5170cc84691bc6be799413c95e22c9
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-09-28 19:06:47 +0530

    Time: 12 ms (81.03%) | Memory: 43.4 MB (81.78%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,41 +1,30 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int count= 0;
+        int count = 0;
         int maxCount = 0;
         int n = s.length();
-        int r = 0;
-        Queue<Character> list = new LinkedList<>();
 
-        while(r<n){
+        for (int r = 0; r < n; r++) {
+            char currChar = s.charAt(r);
 
-            list.add(s.charAt(r));
-            if(list.size() == k+1){
-                if(check(list.peek())){
+            if (check(currChar)) {
+                count++;
+            }
+
+            if (r >= k) {
+                char leftChar = s.charAt(r - k);
+                if (check(leftChar)) {
                     count--;
                 }
-                list.remove();
             }
 
-            if(check(s.charAt(r))){
-                count++;
-                maxCount = Math.max(count,maxCount);
-            }
-            r++;
-        }
-
-        while(!list.isEmpty()){
-            list.remove();
+            maxCount = Math.max(count, maxCount);
         }
 
         return maxCount;
     }
 
-    public boolean check(char c){
-
-        if(c == 'a' || c == 'e'|| c == 'i'|| c == 'o' || c == 'u'){
-            return true;
-        }
-
-        return false;
+    public boolean check(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
