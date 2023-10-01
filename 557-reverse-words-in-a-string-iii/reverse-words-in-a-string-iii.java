de68ed4a0c376f426ed8abb54c0c6218ceb1a795
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-10-01 12:10:29 +0530

    Time: 4 ms (87.56%) | Memory: 44.8 MB (11.59%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,25 +1,36 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String>  q = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++){
-             StringBuilder temp = new StringBuilder();
-            while(i <s.length() && s.charAt(i) != ' '){
-                temp.insert(0,s.charAt(i));
-                i++;
+        StringBuilder result = new StringBuilder();
+        String[] words = s.split(" "); // Split the sentence into words using space as a delimiter
+
+        for (int i = 0; i < words.length; i++) {
+            // Reverse each word and append it to the result
+            result.append(reverseWord(words[i]));
+
+            // Add a space if it's not the last word
+            if (i < words.length - 1) {
+                result.append(" ");
             }
-            q.add(temp.toString());
-            if(i == s.length()){
-                break;
-            }
-            q.add(" ");
         }
 
-        StringBuilder ans = new StringBuilder();
-        while(!q.isEmpty()){
-            ans.insert(0,q.pop());
-            
+        return result.toString();
+    }
+
+    // Helper function to reverse a word
+    private String reverseWord(String word) {
+        char[] charArray = word.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            // Swap characters at the left and right positions
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
         }
 
-        return ans.toString();
+        return new String(charArray);
     }
 }
