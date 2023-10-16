7023f9ea28109b51631e3d969780e9deb53a919a
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-10-16 17:08:34 +0530

    Time: 77 ms (57.92%) | Memory: 54.5 MB (84.52%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -1,26 +1,31 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 class Solution {
     private int[] nums;
+    private HashMap<Integer, List<Integer>> map;
+    private Random random;
 
     public Solution(int[] temp) {
         nums = temp;
+        map = new HashMap<>();
+        random = new Random();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            map.computeIfAbsent(num, k -> new ArrayList<>()).add(i);
+        }
     }
 
     public int pick(int target) {
-        Random rand = new Random();
-        int result = -1;
-        int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                count++;
-                if (rand.nextInt(count) == 0) {
-                    result = i;
-                }
-            }
+        if (map.containsKey(target)) {
+            List<Integer> indices = map.get(target);
+            int randomIndex = random.nextInt(indices.size());
+            return indices.get(randomIndex);
+        } else {
+            return -1; 
         }
-
-        return result;
     }
 }
