99cc912c55df0f877eb4628a4537e29053a9e0ed
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-10-19 12:19:32 +0530

    Time: 2 ms (98.61%) | Memory: 43.2 MB (64.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,20 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
+        int[] count = new int[26];
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
         }
-        int arr[] = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
         }
-
-        for(int i = 0; i < 26; i++){
-            if(arr[i] != 0){
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
                 return false;
             }
         }
-
+        
         return true;
     }
 }
