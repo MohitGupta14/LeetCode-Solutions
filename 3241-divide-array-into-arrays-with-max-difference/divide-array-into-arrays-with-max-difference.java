9fa064d7522bc1d7bf4baf592df21dfa5b0bb8cb
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-12-19 11:42:01 +0530

    Time: 23 ms (92.10%) | Memory: 55.7 MB (60.60%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,42 +1,31 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
 
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
+        int i = 0;
 
-        for (int i = 0; i < nums.length - 2; i = i + 3) {
-            ArrayList<Integer> temp = new ArrayList<>();
-            if (check(nums[i], nums[i + 1], nums[i + 2], k)) {
-                temp.add(nums[i]);
-                temp.add(nums[i + 1]);
-                temp.add(nums[i + 2]);
-                ans.add(temp);
+        while (i < nums.length - 2) {
+            int num1 = nums[i];
+            int num2 = nums[i + 1];
+            int num3 = nums[i + 2];
+
+      if (Math.abs(num1 - num2) <= k && Math.abs(num1 - num3) <= k && Math.abs(num2 - num3) <= k) {
+                ans.add(new int[]{num1, num2, num3});
+                i += 3;
+            } else {
+                i++;
             }
         }
-        if(ans.size() *3 != nums.length){
+
+        if (ans.size() * 3 != nums.length) {
             return new int[0][0];
         }
-        return toArray(ans);
-    }
 
-    public boolean check(int i, int j, int k, int threshold) {
-        if (Math.abs(i - j) <= threshold && Math.abs(i - k) <= threshold && Math.abs(j - k) <= threshold) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public int[][] toArray(ArrayList<ArrayList<Integer>> list) {
-        int[][] array = new int[list.size()][3];
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < 3; j++) {
-                array[i][j] = list.get(i).get(j);
-            }
-        }
-        return array;
+        return ans.toArray(new int[0][0]);
     }
 }
