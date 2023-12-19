00adaf003025f503c00b9205f77dc83d9cc7110e
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-12-19 11:18:29 +0530

    Time: 7 ms (50.42%) | Memory: 44.6 MB (57.42%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -1,53 +1,24 @@
 class Solution {
     public int[][] imageSmoother(int[][] img) {
-        
-        int l = img.length;
-        int ans[][] = new int[l][img[0].length];
-        for(int i = 0 ; i < l ;i++){
-            for(int j = 0 ; j< img[0].length ; j++){
-                int divisor = 0;
-                int sum = 0 ;
+        int m = img.length;
+        int n = img[0].length;
+        int[][] ans = new int[m][n];
 
-                if(i-1 >= 0){
-                    sum += img[i-1][j];
-                    divisor++;
-                    if(j-1 >=0){
-                        sum += img[i-1][j-1]  ;
-                        divisor++;
-                    }
-                    if(j+1 < img[0].length){
-                        sum += img[i-1][j+1] ;
-                        divisor++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                int count = 0;
+
+                for (int ni = i - 1; ni <= i + 1; ni++) {
+                    for (int nj = j - 1; nj <= j + 1; nj++) {
+                        if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                            sum += img[ni][nj];
+                            count++;
+                        }
                     }
                 }
 
-                if(i+1 < l){
-                    sum += img[i+1][j];
-                    divisor++;
-                    if(j-1>=0){
-                        sum += img[i+1][j-1];
-                        divisor++;
-                    }
-                    if(j+1<img[0].length){
-                        sum += img[i+1][j+1];
-                        divisor++;
-                    }
-                }
-
-                if(j-1 >= 0){
-                    sum +=  img[i][j-1];
-                    divisor++;
-                }
-
-                if(j+1 < img[0].length){
-                    sum+= img[i][j+1];
-                    divisor++;
-                }
-
-                sum += img[i][j];  
-                divisor++;
-
-                ans[i][j] = (int)Math.floor((double)sum / divisor);
+                ans[i][j] = (int) Math.floor((double) sum / count);
             }
         }
 
