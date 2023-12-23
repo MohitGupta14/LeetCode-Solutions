e4f6edef2ebbeb9ff377f19df5e5e5c1b3a0d0c0
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-12-23 16:34:29 +0530

    Time: 6 ms (14.98%) | Memory: 42.4 MB (5.29%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        int x = 0;
+        int y = 0;
+        Set<String>s = new HashSet<>();
+        String currentPos = "0,0";
+        for(int i = 0 ; i < path.length() ;i++){
+            s.add(currentPos);
+            if(path.charAt(i) == 'N'){
+                y = y+1;
+        currentPos = String.valueOf(x) + ',' + String.valueOf(y);
+            }
+            else if(path.charAt(i) == 'S'){
+                y = y-1;
+        currentPos = String.valueOf(x) + ',' + String.valueOf(y);
+            }
+            else if(path.charAt(i) == 'E'){
+                x = x+1;
+        currentPos = String.valueOf(x) + ',' + String.valueOf(y);
+            }
+            else if(path.charAt(i) == 'W'){
+                x = x-1;
+        currentPos = String.valueOf(x) + ',' + String.valueOf(y);
+            }
+
+         if(s.contains(String.valueOf(x) + ',' + String.valueOf(y)) ){
+                return true;
+            }
+        
+        }
+
+        System.out.print(y);
+
+        return false;
+    }
+}
