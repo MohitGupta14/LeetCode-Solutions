0a045864af6c40a9f4512bf53312e1376e7c4218
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-12-24 12:21:40 +0530

    Time: 88 ms (5.90%) | Memory: 45.8 MB (5.57%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -4,27 +4,21 @@ class Solution {
         int ans1 = 0;
         int ans2 = 0;
         int n = s.length();
-        char x = s.charAt(0);
         String temp1 = "";
         String temp2 = "";
-        for(int i = 0 ; i<n ;i++){
-            
-            temp1 = temp1 + '0';
-            temp1 = temp1 + '1';
-        
-            temp2 = temp2+ '1';
-            temp2 = temp2 + '0';
 
-            if(s.charAt(i) != temp1.charAt(i)){
+        for(int i = 0; i < n; i++) {
+            temp1 += (i % 2 == 0) ? '0' : '1';
+            temp2 += (i % 2 == 0) ? '1' : '0';
+
+            if(s.charAt(i) != temp1.charAt(i)) {
                 ans1++;
             }
-            if(s.charAt(i) != temp2.charAt(i)){
+            if(s.charAt(i) != temp2.charAt(i)) {
                 ans2++;
             }
-        
         }
 
-
-        return Math.min(ans1,ans2);
+        return Math.min(ans1, ans2);
     }
 }
