4c5046002f3fa139359101d2658f41d63833ecf4
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-12-24 12:30:54 +0530

    Time: 8 ms (7.54%) | Memory: 42.5 MB (6.56%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -4,12 +4,20 @@ class Solution {
         int ans2 = 0;
         int n = s.length();
 
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) != (i % 2 == 0 ? '0' : '1')) {
+        for (int i = 0; i < n; i += 2) {
+            if (s.charAt(i) != '0') {
                 ans1++;
             }
 
-            if (s.charAt(i) != (i % 2 == 0 ? '1' : '0')) {
+            if (i+1 < n && s.charAt(i + 1) != '1') {
+                ans1++;
+            }
+
+            if (s.charAt(i) != '1') {
+                ans2++;
+            }
+
+            if (i+1 <n && s.charAt(i + 1) != '0') {
                 ans2++;
             }
         }
