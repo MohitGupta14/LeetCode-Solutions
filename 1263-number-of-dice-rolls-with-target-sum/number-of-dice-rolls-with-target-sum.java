6e52439fa34ec0893a7044c71d5b291dff447ccc
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-12-26 11:03:23 +0530

    Time: 6 ms (94.31%) | Memory: 41.5 MB (29.72%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -7,13 +7,16 @@ class Solution {
 
         for (int i = 1; i <= n; i++) {
             int[] newDp = new int[target + 1];
+            int sum = 0;
+
             for (int j = 1; j <= target; j++) {
-                for (int d = 1; d <= k; d++) {
-                    if (j - d >= 0) {
-                        newDp[j] = (newDp[j] + dp[j - d]) % MOD;
-                    }
+                sum = (sum + dp[j - 1]) % MOD;
+                if (j >= k + 1) {
+                    sum = (sum - dp[j - k - 1] + MOD) % MOD;
                 }
+                newDp[j] = sum;
             }
+
             dp = newDp;
         }
 
