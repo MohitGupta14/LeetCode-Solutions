0009b5b40a9031f422b91d9328a87ce9aa6b3a48
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2025-02-01 22:42:43 +0530

    Time: 5 ms (74.09%) | Memory: 57.8 MB (72.24%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,23 +1,16 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxVol = 0 ;
-         
-        int ptr1 = 0;
-        int ptr2 = height.length-1;
+        int maxVol = 0;
+        int ptr1 = 0, ptr2 = height.length - 1;
 
-        while(ptr1<height.length-1){
-            if(height[ptr1]< height[ptr2]){
-                maxVol = Math.max(maxVol,height[ptr1]*(ptr2-ptr1));
-                ptr1++;
-                ptr2 = height.length-1;
-            }else{
-               maxVol = Math.max(maxVol,height[ptr2]*(ptr2-ptr1));
-               ptr2--;
-            }
+        while (ptr1 < ptr2) {
+            int minHeight = Math.min(height[ptr1], height[ptr2]);
+            maxVol = Math.max(maxVol, minHeight * (ptr2 - ptr1));
 
-            if(ptr2<=ptr1){
+            if (height[ptr1] < height[ptr2]) {
                 ptr1++;
-                ptr2 = height.length-1;
+            } else {
+                ptr2--;
             }
         }
 
