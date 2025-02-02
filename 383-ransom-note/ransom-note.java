a970ab5914a3417f7a073f9e9d9c692b0e8873c5
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2025-02-02 13:35:23 +0530

    Time: 13 ms (37.56%) | Memory: 45.2 MB (51.54%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(Character s : magazine.toCharArray()){
+            map.put(s,map.getOrDefault(s,0)+1);
+        }
+
+        for(Character s : ransomNote.toCharArray()){
+            if(map.containsKey(s) && map.get(s) != 0){
+                map.put(s,map.getOrDefault(s,0)-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
