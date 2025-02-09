a4fbe6670928d8d57e6a502882edfe55096bca44
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2025-02-09 16:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (38.79%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,42 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private List<Integer> store = new ArrayList<>();
+    private Integer prev = null; // Stores the previous node value
 
     public boolean isValidBST(TreeNode root) {
-        inorderHelper(root);
+        return inorderHelper(root);
+    }
 
-        for(int i = 1 ; i < store.size(); i++){
-            if(store.get(i-1) >= store.get(i)){
-                return false;
-            }
+    private boolean inorderHelper(TreeNode node) {
+        if (node == null) return true;
+
+     
+        if (!inorderHelper(node.left)) return false;
+
+        if (prev != null && node.val <= prev) {
+            return false;
         }
+        prev = node.val; 
 
-        return true;
+        return inorderHelper(node.right);
     }
-  
-
-    private void inorderHelper(TreeNode node) {
-        if (node == null) return;
-
-        inorderHelper(node.left); 
-
-        store.add(node.val);
-
-        inorderHelper(node.right); 
-    }
-
 }
